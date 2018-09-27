efcf5b5c5ffc33b2cd7841eec305c7c6b3f4ddcd
Roboronnie <leex4920@gmail.com>
2018-09-27 06:44:10 -0500

    toggle state variable between true and false when clicking add more rep button

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      moreRepresentative: false,
+    }
+  }
+
+  addMoreRepresentatives = () => {
+    this.setState({
+      moreRepresentative: !this.state.moreRepresentative
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Christy's Listy</h1>
         </header>
-        
-
         <p>Enter Company Name and Representative(s)</p>
         <label>Company Name: </label>
         <input type="text" placeholder="Company Name"/>
@@ -17,7 +28,7 @@ class App extends Component {
         <label>Representative: </label>
         <input type="text" placeholder="Representative"/>
         <br/>
-        <button>Add More Representative</button>
+        <button onClick={this.addMoreRepresentatives}>Add More Representative</button>
       </div>
     );
   }
