2733e776a6c4c2710f0635f5483180c985ba426d
Roboronnie <leex4920@gmail.com>
2018-10-06 09:55:48 -0500

    add student list to database via student.router

--- server/routes/student.router.js
+++ server/routes/student.router.js
@@ -3,8 +3,16 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.post('/', (req,res) => {
-    console.log(req.body);
-    console.log(`inside '/api/student'`);
+    for(let i = 0; i < req.body.length; i++){
+        const queryText = `INSERT INTO students (student) VALUES ($1);`;
+        const queryValues = [req.body[i].name];
+        pool.query(queryText,queryValues)
+            .then(() => { res.sendStatus(201); })
+            .catch((error) => {
+                res.sendStatus(500);
+                console.log('error posting new students', error);
+            });
+    };
 });
 
 module.exports = router;
