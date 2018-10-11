ed80202e7960dd84be940d2df56637ad5bd4f256
Roboronnie <leex4920@gmail.com>
2018-10-10 23:44:08 -0500

    insert company name into db company table

--- server/routes/companyAndRepresentative.router.js
+++ server/routes/companyAndRepresentative.router.js
@@ -4,6 +4,22 @@ const router = express.Router();
 
 router.post('/', async (req,res) => {
     console.log('reached server router for company and representatives');
+    console.log(req.body);
+    // { company: 'homi',
+//   representatives: [ { name: 'kevin' }, { name: 'mai yer' } ] }
+    console.log(req.body.company);
+    console.log(req.body.representatives);
+    const queryText = `INSERT INTO company (name) VALUES ($1);`;
+    const queryValue = [req.body.company];
+    pool.query(queryText,queryValue)
+        .then(() => {
+            console.log('here are the representatives: ', req.body.representatives);
+            // select company ID from company table and insert rep into rep table using company ID 
+         })
+        .catch((error) => {
+            res.sendStatus(500);
+            console.log('error posting new company', error);
+        });
 });
 
 module.exports = router;
