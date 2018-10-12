db4d73f4c01fa43752c0a8f497d0b09fffca512f
Roboronnie <leex4920@gmail.com>
2018-10-12 16:20:00 -0500

    split company and rep into two seperate steps in generator function in saga

--- server/routes/companyAndRepresentative.router.js
+++ server/routes/companyAndRepresentative.router.js
@@ -2,24 +2,32 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-router.post('/', async (req,res) => {
-    console.log('reached server router for company and representatives');
-    console.log(req.body);
-    // { company: 'homi',
-//   representatives: [ { name: 'kevin' }, { name: 'mai yer' } ] }
-    console.log(req.body.company);
-    console.log(req.body.representatives);
+router.post('/company', (req,res) => {
     const queryText = `INSERT INTO company (name) VALUES ($1);`;
     const queryValue = [req.body.company];
     pool.query(queryText,queryValue)
-        .then(() => {
-            console.log('here are the representatives: ', req.body.representatives);
-            // select company ID from company table and insert rep into rep table using company ID 
-         })
-        .catch((error) => {
+        .then(()=>{
+            res.sendStatus(201);
+        }).catch((error)=>{
             res.sendStatus(500);
-            console.log('error posting new company', error);
+            console.log('error posting to company: ', error);
         });
+});
+
+router.post('/:company', (req,res) => {
+    console.log('reached server router for company and representatives');
+    console.log(req.body); // representatives
+    console.log(req.params); // company
+    // get company ID 
+    pool.query(`SELECT company.id as id from company where company.name = '${req.params}'`)
+        .then((result)=>{
+            console.log('result.rows: ',result.rows); //currently empty array
+        }).catch((error)=>{
+            console.log('error selecting company id: ', error);
+        });
+    // insert into rep table with company ID
+    // const queryText = `INSERT INTO representatives (name, company_id) VALUES ($1, $2);`;
+    // const queryValue = [req.body.company];
 });
 
 module.exports = router;
--- src/redux/sagas/companyAndRepresentativeSaga.js
+++ src/redux/sagas/companyAndRepresentativeSaga.js
@@ -2,15 +2,30 @@ import { takeEvery, call, put as dispatch } from 'redux-saga/effects';
 import axios from 'axios';
 
 function* companyAndRepresentativeSaga() {
+    // yield takeEvery('POST_COMPANY', postCompany);
     yield takeEvery('POST_COMPANY_AND_REPRESENTATIVES', postCompanyAndRepresentative);
 }
+
+// function* postCompany(action) {
+//     try {
+//         yield call(axios.post, 'api/companyAndRepresentatives', action.payload);
+//     } catch (error) {
+//         console.log('error company saga: ', error);
+//     }
+// }
+
 
 function* postCompanyAndRepresentative(action) {
     console.log('inside postCompanyAndRepresentative');
     try {
-        yield call(axios.post, '/api/companyAndRepresentatives', action.payload);
+        yield call(axios.post, 'api/companyAndRepresentatives/company', {company: action.payload.company});
     } catch (error) {
-        console.log('error company and representative saga: ', error);
+        console.log('error company saga: ', error);
+    }
+    try {
+        yield call(axios.post, `/api/companyAndRepresentatives/${action.payload.company}`, {representatives: action.payload.representatives});
+    } catch (error) {
+        console.log('error representative saga: ', error);
     };
 }
 
